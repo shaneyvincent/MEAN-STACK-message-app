5c50d9205e29ba1eb1dbdcca8020100797beaac7
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-03 00:08:51 -0400

    added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
-var database;
+var Message = mongoose.model('Message',{
+    msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -11,19 +13,29 @@ app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
-
 })
 
 app.post('/api/message', function(req,res){
-              console.log(req.body);
-              database.collection('messages').insertOne(req.body);
-              res.status(200);
+    console.log(req.body);
+
+    var message = new Message(req.body);
+
+    message.save();
+
+    res.status(200);
 })
 
-mongo.connect("mongodb://localhost:27017/test", function(err,db){
+function GetMessages()
+{
+    Message.find({}).exec(function(err, result){
+        console.log(result);
+    })
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err,db){
   if(!err){
     console.log("we are conected to mongo");
-    database = db;
+    GetMessages();
   }
 })
 
