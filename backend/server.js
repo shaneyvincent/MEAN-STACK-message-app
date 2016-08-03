c5c61edfd0df4797d6464b0141ec7372125c9603
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-02 22:45:22 -0400

    save content to mongo from back end point

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+
+var database;
 
 app.use(bodyParser.json());
 
@@ -8,12 +11,20 @@ app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
-    
+
 })
 
 app.post('/api/message', function(req,res){
-  console.log(req.body);
-  res.status(200);
+              console.log(req.body);
+              database.collection('messages').insertOne(req.body);
+              res.status(200);
+})
+
+mongo.connect("mongodb://localhost:27017/test", function(err,db){
+  if(!err){
+    console.log("we are conected to mongo");
+    database = db;
+  }
 })
 
 var server = app.listen(5000, function(){
