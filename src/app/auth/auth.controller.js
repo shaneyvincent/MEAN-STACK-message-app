f70230da5376b51efc85a02dbc0cb41192d1307b
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-03 18:36:14 -0400

    appended module to include auth controller

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,21 +1,4 @@
 export class Auth Controller {
-  constructor ($http) {
-    'ngInject';
 
-      this.$http = $http;
-      this.getMessages();
-
-  }
-
-      getMessages() {
-        var vm = this;
-        this.$http.get('http://localhost:5000/api/message').then(function(result){
-            vm.messages = result.data;
-        });
-
-      }
-      postMessage() {
-        this.$http.post('http://localhost:5000/api/message', {msg: this.message});
-      }
 
     }
