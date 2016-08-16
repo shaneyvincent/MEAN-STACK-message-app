358dcc547ab0c1885eb30806f36d051d23f69f03
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-16 12:43:20 -0400

    prevented user from signing up with the same email

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,20 +1,27 @@
 var User = require('../models/user');
 
-
 module.exports = {
     register: function(req,res){
             console.log(req.body);
 
+            User.findOne({
+              email: req.body.email
+            }, function(err, existingUser){
+
+              if(existingUser)
+                  return res.status(409).send({message: 'Email is already registered fool!'});
+
+
             var user = new User(req.body);
 
             user.save(function(err,result){
-              if(err)
-                  {
+              if(err) {
                     res.status(500).send({
                       message: err.message
                     });
                   }
                   res.status(200);
             })
+          });
     }
 }
