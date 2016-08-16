db145db69190842a452af78f33e1f9584cee5959
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-16 12:55:12 -0400

    created Token for browser to accept

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,6 @@
 var User = require('../models/user');
+var jwt = require('jwt-simple');
+var moment = require('moment');
 
 module.exports = {
     register: function(req,res){
@@ -20,8 +22,17 @@ module.exports = {
                       message: err.message
                     });
                   }
-                  res.status(200);
+                  res.status(200).send({token: createToken(result)});
             })
           });
     }
+}
+
+function createToken(user){
+  var payload = {
+    sub: user.id,
+    iat: moment().unix(),
+    exp: moment().add(14, 'days').unix()
+  };
+  return jwt.encode(payload,'secret');
 }
