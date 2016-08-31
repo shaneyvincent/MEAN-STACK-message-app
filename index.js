f23c52d49a9af0ea52c8f0dd94dfbdcd05db4c4d
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-30 20:50:40 -0400

    test main

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('app', __dirname + '/app');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('main/main');
 });
 
 app.listen(app.get('port'), function() {
