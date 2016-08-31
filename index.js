6abe859548b475d7411992dd2f10e54c5eee9155
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-30 20:58:21 -0400

    putting things back

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('app', __dirname + '/app');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('main/main');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
