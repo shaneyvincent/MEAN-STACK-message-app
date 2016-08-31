2e6b6a99017bafdc89e1cc99d3ccd1c16cc8bb31
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-08-30 21:18:27 -0400

    modified indexejs

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index.route');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
